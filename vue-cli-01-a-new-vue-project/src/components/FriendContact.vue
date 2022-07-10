8034905c0dab3a4081d80b693fc4e23378637f39
chituanh <51024097+chituanh@users.noreply.github.com>
2022-07-10 18:26:23 +0700

    xong section 8

---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,34 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails" >Show detail</button>
+        <ul v-if="detailAreVisible" >
+            <li><strong>Phone: </strong> {{friend.phone}} </li>
+            <li><strong>Email: </strong> {{friend.email}} </li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    setup() {
+        
+    },
+    data() {
+        return {
+            detailAreVisible: false,
+            friend: {
+                id: "manuel",
+                name: "Mesdklf sdlfk sdf",
+                phone: 'heheee',
+                email: 'huhuhu',
+            },
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.detailAreVisible = !this.detailAreVisible;
+        }
+    }
+}
+</script>
